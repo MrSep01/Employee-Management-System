82fe7cf706941126acad6a7525def9c163546078
sep <sep.alamouti@sepalamouti.com>
2023-10-18 21:18:07 -0700

    adding-inquirer-questions

--- Main/server.js
+++ Main/server.js
@@ -7,4 +7,23 @@ const connection = connect();  // Get the connection object using the connect fu
 const sqlFunctions = new SqlFunctions(connection);
 
 
-
+async function main() {
+  const answer = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Update employee manager',
+        'View employees by manager',
+        'View employees by department',
+        'Delete a department',
+        'View department budget',
+        'Exit'
+      ]
+    }
+  ]);
